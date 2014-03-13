dc93104f69cc9d14822c744a417e46435e54d567
OddurV <oddurvilhj@gmail.com>
2014-03-13 08:22:27 +0000

    Smávægileg uppfærsla

--- Solitaire/Prompt.py
+++ Solitaire/Prompt.py
@@ -3,13 +3,17 @@
 #Tilkynningar til, og skipanir frá notanda
 
 #Fall sem prentar stöðuna í leiknum
-def Stada(self, Spilari):
+def Stada(Spilari):
     pass
 
 #Fall sem segir forritinu að hætta leiknum
-def Stoppa(self, Spilari):
+def Stoppa(Spilari):
     pass
 
 #Fall sem skilgreinir terminalspilun og leikjalúppu fyrir Spilara, notar öll promptföllin
-def Terminalspilun(self, Spilari):
+def Terminalspilun(Spilari):
     pass
+
+#Fall sem prentar út "Ólöglegur leikur"
+def Villa():
+	pass
