94a75e7c29d7849d9a87b012a17a136736c28931
Ossur <ossur.ingi@gmail.com>
2014-04-03 13:05:59 +0000

    Uppfærði tóma dagbók með nýjum dagsetningum og bjó til skil4 folderinn

--- Solitaire/gui_alpha.py
+++ Solitaire/gui_alpha.py
@@ -26,17 +26,23 @@ class MyFrame(wx.Frame):
             pos=(10, 348), size = (image1.GetWidth()+5, image1.GetHeight()+5))
         self.btn.Bind(wx.EVT_LEFT_DOWN, self.B1Click)
         
+       
+        
         self.delta = ((0, 0))
         self.Bind(wx.EVT_LEFT_DOWN, self.onDown)
         self.btn.Bind(wx.EVT_LEFT_DOWN, self.onButton)
         self.btn.Bind(wx.EVT_MOTION, self.onMove)
         self.btn.Bind(wx.EVT_LEFT_UP, self.onRelease)
+        self.btn.Bind(wx.EVT_LEAVE_WINDOW, self.onLeave)
+        self.panel.Bind(wx.EVT_KEY_UP, self.upPressed) 
+        
+        self.panel.SetFocus()
         
         #Icon fyrir main glugga
         path = os.path.abspath("./Myndir/icon.jpeg")
         icon = wx.Icon(path, wx.BITMAP_TYPE_JPEG)
         self.SetIcon(icon)
-
+        
         #Menu bar fyrir options
         menu_bar = wx.MenuBar()
 
@@ -49,6 +55,11 @@ class MyFrame(wx.Frame):
         options.Append(wx.NewId(), "Highscore")
         options.Append(wx.NewId(), "Rules")
         self.SetMenuBar(menu_bar)
+        
+        #Add sizer
+        panelSizer = wx.BoxSizer(wx.VERTICAL) 
+        panelSizer.Add(self.panel, 1, wx.EXPAND)
+        self.SetSizerAndFit(panelSizer)
 
 
     def B1Click(self,event):
@@ -58,24 +69,39 @@ class MyFrame(wx.Frame):
         event.Skip()
 
     def onButton(self, event):
+        print "buttpressstart"
         self.btn.CaptureMouse()
         x, y = self.ScreenToClient(self.btn.ClientToScreen(event.GetPosition()))
         originx, originy = self.btn.GetPosition()
         dx = x - originx
         dy = y - originy
         self.delta = ((dx, dy))
+        print "buttpressdone"
+        
 
     def onMove(self, event):
         if event.Dragging():
+            print "dragstart"
             x, y = self.ScreenToClient(self.btn.ClientToScreen(event.GetPosition()))
             fp = (x-self.delta[0], y-self.delta[1])
             self.btn.Move(fp)
 
     def onRelease(self, event):
+        print "onreleasestart"
         if self.btn.HasCapture():
+            print "onreleasestarthascaptures"
             self.btn.ReleaseMouse()
-
+            print "onreleasereleasemousedone"
             
+    def onLeave(self, event):
+        print "onleavestart"
+        if self.btn.HasCapture():
+            print "onleavestarthascapture"
+            self.btn.ReleaseMouse()
+            print "onleavereleasemousedone"
+    def upPressed(self, event):
+        print "uppressed"
+                
 if __name__ == "__main__":
     app = MyApp(False)
     app.MainLoop()
