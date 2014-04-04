d00df8cf7ac5fe2208816e1afca8611b120c549a
KristjanGunn <thepurplepeopleeater89@gmail.com>
2014-04-04 13:46:32 +0000

    Uppfærð reglubók

--- Solitaire/Reglubok.py
+++ Solitaire/Reglubok.py
@@ -7,11 +7,34 @@ screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 done = False
 
-font = pygame.font.SysFont("comicsansms", 40)
-#font_path = 'data/fonts/menu_font.ttf'
-#font = pygame.font.Font
+myfont = pygame.font.SysFont("monospace", 15)
 
-text = font.render("Hello, World", True, (0, 128, 0))
+# render text
+label = myfont.render("Markmið spilara er að setja spil stokks í fjóra stafla, þar sem öll", 1, (255,255,0))
+screen.blit(label, (10, 10))
+label = myfont.render("spil hvers stafla eru af sömu sort og voru sett þangað í þrepvaxandi", 1, (255,255,0))
+screen.blit(label, (10, 25))
+label = myfont.render("röð.", 1, (255,255,0))
+screen.blit(label, (10, 40))
+
+label = myfont.render("Ás flokkast sem lægsta spilið og fyrsta spil hvers stafla verður að", 1, (255,255,0))
+screen.blit(label, (10, 65))
+label = myfont.render("vera ás. Spil stokks snúa alltaf niður.", 1, (255,255,0))
+screen.blit(label, (10, 80))
+
+label = myfont.render("Bunkarnir eru af stærð 1 til 7 í þrepvaxandi röð frá hægri til vinstri.", 1, (255,255,0))
+screen.blit(label, (10, 105))
+label = myfont.render("Spil bunka snúa niður en ef efsta spil bunka snýr niður má snúa því upp.", 1, (255,255,0))
+screen.blit(label, (10, 120))
+
+label = myfont.render("Spilari má taka efsta spil stokks og setja á stafla ef það er hægt.", 1, (255,255,0))
+screen.blit(label, (10, 145))
+label = myfont.render("Hann má líka setja spilið í annan stafla - svokallaðan ruslastafla. Ef", 1, (255,255,0))
+screen.blit(label, (10, 160))
+label = myfont.render("stokkurinn er búinn má endurvinna ruslastaflann, en það þýðir að snúa honum", 1, (255,255,0))
+screen.blit(label, (10, 175))
+label = myfont.render("við og endurnota sem stokk.", 1, (255,255,0))
+screen.blit(label, (10, 190))
 
 while not done:
     for event in pygame.event.get():
@@ -20,10 +43,12 @@ while not done:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             done = True
     
-    screen.fill((255, 255, 255))
-    screen.blit(text,
-        (320 - text.get_width() // 2, 240 - text.get_height() // 2))
+ #   screen.fill((255, 255, 255))
+  #  screen.blit(text,
+   #     (320 - text.get_width() // 2, 240 - text.get_height() // 2))
     
     pygame.display.flip()
     clock.tick(60)
 
+#                          
+# 
